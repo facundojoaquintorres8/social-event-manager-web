1ea89e1ff4ee0dbe92b047e41b08e157ab0d2759
Facundo Torres <ftorres@ubicuagroup.com>
2026-05-10 14:43:00 -0300

    button dark mode in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,13 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastService } from './core/services/toast.service';
 import { CommonModule } from '@angular/common';
-import { ThemeService } from './core/services/theme.service';
-import { LucideAngularModule, Moon, Sun } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, LucideAngularModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   readonly toastService = inject(ToastService);
-  readonly themeService = inject(ThemeService);
-
-  readonly Moon = Moon;
-  readonly Sun = Sun;
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { LucideAngularModule, LogOut } from 'lucide-angular';
+import { LucideAngularModule, LogOut, Moon, Sun } from 'lucide-angular';
+import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,10 @@ import { LucideAngularModule, LogOut } from 'lucide-angular';
 export class HeaderComponent {
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  readonly themeService = inject(ThemeService);
 
+  readonly Moon = Moon;
+  readonly Sun = Sun;
   readonly LogOut = LogOut;
 
   logout(): void {
