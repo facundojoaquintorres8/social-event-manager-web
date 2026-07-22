7f85bf1bcb9bad52d7a784b8f7ab1dada1ee5415
Facundo Torres <ftorres@ubicuagroup.com>
2026-07-22 20:26:08 -0300

    error interceptor translation

--- src/app/core/interceptors/error-interceptor.ts
+++ src/app/core/interceptors/error-interceptor.ts
@@ -2,20 +2,22 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { ToastService } from '../services/toast.service';
+import { TranslateService } from '@ngx-translate/core';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toastService = inject(ToastService);
+  const translate = inject(TranslateService);
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 0) {
-        toastService.show('Unable to connect to server', 'error');
+        toastService.show(translate.instant('errors.connectionError'), 'error');
       } else if (error.error?.message) {
         toastService.show(error.error.message, 'error');
       } else if (error.status >= 500) {
-        toastService.show('Internal server error', 'error');
+        toastService.show(translate.instant('errors.serverError'), 'error');
       } else {
-        toastService.show('Unexpected error occurred', 'error');
+        toastService.show(translate.instant('errors.unexpectedError'), 'error');
       }
 
       return throwError(() => error);
